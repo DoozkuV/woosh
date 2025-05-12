8ca9eb509a3a51ea07f721a1324333e9aab6aca0
George Padron <georgenpadron@gmail.com>
2025-05-12 12:48:45 -0500

    Added more tests in the "main" func

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use woosh::parser::parse;
 
 fn main() {
-    let input = "echo hello world | sed 's/h/H/g' |grep foo > out.txt";
+    print_parse("echo hello world | sed 's/h/H/g' |grep foo > out.txt");
+    print_parse("echo 'good morning!'");
+}
+
+fn print_parse(input: &str) {
     match parse(input) {
         Ok(ast) => {
             println!("Parsed AST: {:#?}", ast);
